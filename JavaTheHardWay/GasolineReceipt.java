ca129b2936bf7b24babb9d5390b0d762ee556376
Nathaniel Trainor <natetrainor@msn.com>
2018-02-09 13:25:06 -0700

    Finished jthw excercise 2

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,13 @@
+public class GasolineReceipt {
+  public static void main ( String[] args) {
+    System.out.println( "+----------------+" );
+    System.out.println( "|                |" );
+    System.out.println( "|  CORNER STORE  |" );
+    System.out.println( "|   2018-03-29   |" );
+    System.out.println( "| Gallons 10.945 |" );
+    System.out.println( "|Price/gallon: $2|" );
+    System.out.println( "|Fuel total: $21 |" );
+    System.out.println( "|                |" );
+    System.out.println( "+----------------+" );
+  }
+}
