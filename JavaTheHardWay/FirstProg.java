de6d1f6ec765395e6f7e5ade4e7812f65ba581d0
Nathaniel Trainor <natetrainor@msn.com>
2018-02-09 13:11:03 -0700

    Finished study drills 1

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -1,8 +1,8 @@
 public class FirstProg {
 
   public static void main( String[] args) {
-    System.out.println( "I am determined to learn how to code." );
-    System.out.println( "Today's date is" );
+    System.out.println( "I, Nate Trainor, am determined to learn how to code." );
+    System.out.println( "Today's date is Feburary 9, 2018." );
   }
-  
+
 }
