c3dd5ca0dc5d1930dc6419e60df2be8e3001c887
Nathaniel Trainor <natetrainor@msn.com>
2018-02-09 14:08:06 -0700

    Finished Lab 1

---
+++ Labs/Objective1Lab1.java
@@ -0,0 +1,11 @@
+public class Objective1Lab1 {
+  public static void main(String[] args){
+
+    /*
+      Here is my solution!
+    */
+
+    System.out.println("Hello, World. I am Nate Trainor.");
+
+  }
+}
---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,8 @@
+public class Objective1Lab3 {
+  public static void main(String[] args) {
+    System.out.println("Name: " + "Nate Trainor");
+    System.out.println("Age: " + "22");
+    System.out.print("State: ");
+    System.out.println("Colorado");
+  }
+}
