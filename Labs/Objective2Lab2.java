030dfaa0dced4e4dd882ff3514fc348592a4eb7b
Nathaniel Trainor <natetrainor@msn.com>
2018-02-09 19:51:48 -0700

    Completed Objective 2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,12 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Nate Trainor";
+    int age = 22;
+    String state = "Colorado";
+
+    System.out.print("Name: " + name);
+    System.out.print(", Age: " + age);
+    System.out.println(", State: " + state);
+
+  }
+}
---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,14 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    String food1, food2, food3;
+
+    food1 = "Salmon";
+    food2 = "Mashed Potatoes";
+    food3 = "Pasta";
+
+    System.out.println("I like food. " + food1 + " is my favorite.");
+    System.out.println(food2 + " is a good side dish.");
+    System.out.println(food3 + " is my second favorite dish.");
+
+  }
+}
